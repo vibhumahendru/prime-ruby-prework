f7b42278a51e3a098ab9295269e0c1b1aa4fbd94
vibhu mahendru <vibhumahendru@gmail.com>
2018-10-04 00:55:24 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,16 @@
 # Add  code here!
+def prime?(num)
+  flag = true
+  if num<=1
+    return false
+  end
+  for x in 2..num-1
+  if num%x == 0 
+    flag = false 
+ end
+
+end
+ return flag
+end
+
+  
